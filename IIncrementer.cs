96e1b83f09fe24dd5f85fa4cc873478e8d6e713c
Константин Дьяченко <konard@me.com>
2019-10-22 16:08:24 +0300

    Closes #4.

---
+++ IIncrementer.cs
@@ -0,0 +1,15 @@
+﻿namespace Platform.Incrementers
+{
+    /// <summary>
+    /// <para>Defines an incrementer that increments any object.</para>
+    /// <para>Определяет инкрементер, который выполняет приращение любого объекта.</para>
+    /// </summary>
+    public interface IIncrementer
+    {
+        /// <summary>
+        /// <para>Increments the object by a specific value.</para>
+        /// <para>Выполняет приращение объекта на определённое значение.</para>
+        /// </summary>
+        void Increment();
+    }
+}
