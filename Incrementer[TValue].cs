eba7f3bb22c61bfaf154ff15bfc209d90df184db
Константин Дьяченко <konard@me.com>
2019-10-22 16:25:09 +0300

    This pragma is not needed anymore.

--- Incrementer[TValue].cs
+++ Incrementer[TValue].cs
@@ -1,6 +1,5 @@
 ﻿using System.Runtime.CompilerServices;
 
-#pragma warning disable IDE0060 // Remove unused parameter
 #pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
 
 namespace Platform.Incrementers
