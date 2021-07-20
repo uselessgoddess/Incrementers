28ac0366b4c7ff2f2a0e82d2dd6636466cc5b113
Mitron57 <85793630+Mitron57@users.noreply.github.com>
2021-07-20 23:10:38 +0400

    auto

--- cpp/Platform.Incrementers.Tests/IncrementerTests.cpp
+++ cpp/Platform.Incrementers.Tests/IncrementerTests.cpp
@@ -5,19 +5,19 @@ namespace Platform::Incrementers::Tests
 {
     TEST(IncrementersTests, ParameterlessConstructedSetterTest)
     {
-        Incrementer<int> incrementer = Incrementer<int>();
+        auto incrementer = Incrementer<int>();
         ASSERT_EQ({}, incrementer.Result());
     }
 
     TEST(IncrementersTests, ConstructedWithDefaultValueSetterTest)
     {
-        Incrementer<int> incrementer = Incrementer<int>(9UL);
+        auto incrementer = Incrementer<int>(9UL);
         ASSERT_EQ(9UL, incrementer.Result());
     }
 
     TEST(IncrementersTests, MethodsWithBooleanReturnTypeTest)
     {
-        Incrementer<int> incrementer = Incrementer<int>();
+        auto incrementer = Incrementer<int>();
         incrementer.Increment();
         ASSERT_EQ(1UL, incrementer.Result());
         ASSERT_TRUE(incrementer.IncrementAndReturnTrue());
@@ -26,7 +26,7 @@ namespace Platform::Incrementers::Tests
 
     TEST(IncrementersTests, MethodsWithIntegerReturnTypeTest)
     {
-        Incrementer<int, int> incrementer = Incrementer<int, int>(1);
+        auto incrementer = Incrementer<int, int>(1);
         incrementer.Increment();
         ASSERT_EQ(1UL, incrementer.Result());
         ASSERT_EQ(1, incrementer.IncrementAndReturnTrue());
