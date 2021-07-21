ae83ac2e21b9b9120b21a1fd066a474604090656
Mitron57 <85793630+Mitron57@users.noreply.github.com>
2021-07-21 18:27:36 +0400

    Unification

--- cpp/Platform.Incrementers/Incrementer.h
+++ cpp/Platform.Incrementers/Incrementer.h
@@ -1,18 +1,64 @@
 ﻿namespace Platform::Incrementers
 {
-    template<typename...> class Incrementer;
-    template<> class Incrementer<>
+    template<typename TValue = std::size_t, typename TDecision = bool>
+    class Incrementer
     {
+        using base = Incrementer;
+        private: const TDecision _trueValue {};
+
+        template<typename decision_type>
+        constexpr auto true_or_default()
+        {
+            if constexpr (std::integral<decision_type>)
+            {
+                return decision_type{1};
+            }
+            else
+            {
+                return decision_type{};
+            }
+        }
+
         protected: std::size_t _result {};
 
         public: [[nodiscard]] auto Result() const noexcept { return _result; }
+
+        public: Incrementer() = default;
 
         public: explicit Incrementer(std::size_t initialValue) : _result(initialValue)
         {
         }
 
-        public: Incrementer() = default;
+        public: explicit Incrementer(std::size_t initialValue, TDecision trueValue) : base(initialValue), _trueValue(trueValue)
+        {
+        }
+
+        public: explicit Incrementer(TValue value = {}, TDecision decision = true_or_default<TDecision>()): _trueValue(trueValue)
+        {
+        }
 
         public: void Increment() noexcept { _result++; }
+
+        public: TDecision IncrementAndReturnTrue()
+        {
+            _result++;
+            return _trueValue;
+        }
+
+        public: TDecision IncrementAndReturnTrue(TValue value)
+        {
+            _result++;
+            return _trueValue;
+        }
+
+        /*using anotherbase = Incrementer<TValue, bool>;
+
+        public: explicit Incrementer(std::size_t initialValue) : anotherbase(initialValue, true)
+        {
+        }
+
+        public: Incrementer() : anotherbase(true)
+        {
+        }*/
     };
 }
--- cpp/Platform.Incrementers/Incrementer[TValue].h
+++ cpp/Platform.Incrementers/Incrementer[TValue].h
@@ -1,16 +1,1 @@
-﻿namespace Platform::Incrementers
-{
-    template<typename...> class Incrementer;
-    template<typename TValue> class Incrementer<TValue> : public Incrementer<TValue, bool>
-    {
-        using base = Incrementer<TValue, bool>;
-
-        public: explicit Incrementer(std::uint64_t initialValue) : base(initialValue, true)
-        {
-        }
-
-        public: Incrementer() : base(true)
-        {
-        }
-    };
-}
+﻿
