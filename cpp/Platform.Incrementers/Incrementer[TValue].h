11c6e823c9bb64956910c1439c28c15860ba949d
Mitron57 <85793630+Mitron57@users.noreply.github.com>
2021-07-21 17:22:10 +0400

    Update cpp/Platform.Incrementers/Incrementer[TValue].h
    Co-authored-by: uselessgoddess <68294279+uselessgoddess@users.noreply.github.com>

--- cpp/Platform.Incrementers/Incrementer[TValue].h
+++ cpp/Platform.Incrementers/Incrementer[TValue].h
@@ -3,7 +3,7 @@
     template<typename...> class Incrementer;
     template<typename TValue> class Incrementer<TValue> : public Incrementer<TValue, bool>
     {
-        private: using base = Incrementer<TValue, bool>;
+        using base = Incrementer<TValue, bool>;
 
         public: explicit Incrementer(std::uint64_t initialValue) : base(initialValue, true)
         {
